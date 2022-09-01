54c4e1bbb0acc3ff2c777c43375fe2fe20bc0cc8
Andrii Reznik <icandrii.reznik@americanwell.com>
2022-09-01 13:08:05 +0200

    fix dots version

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 30,
-    autoWidth: true,
+    autoWidth: false,
     items: 5,
     nav: true,
-    navText: ['<', '>'],
     responsive: {
       0: {
         items: 1
